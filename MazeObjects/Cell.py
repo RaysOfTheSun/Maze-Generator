b6f39df30f5e9d972decd2e30c9642c275559f4c
RaysOfTheSun <rdelsol35@gmail.com>
2018-04-24 18:44:44 +0800

    update classes
    + Added methods needed to obtain the neighbors of the current cell
    + Added required step in algo

--- MazeObjects/Cell.py
+++ MazeObjects/Cell.py
@@ -1,5 +1,6 @@
 import pygame
 from MazeObjects.Colors import Color
+import random
 
 
 class Cell:
@@ -41,14 +42,37 @@ class Cell:
             pygame.draw.line(surface, self.__palette.white, ((x + self.__width), y),
                              ((x + self.__width), (y + self.__width)))
 
-        rectangle = pygame.Surface((self.__width, self.__width))
-        rectangle.fill(self.__palette.purple)
-        rectangle.set_alpha(50)
-        # surface.blit(rectangle, (self.__x_coordinate, self.__y_coordinate))
-        surface.blit(rectangle, (0, 0))
+        if self.visited:
+            rectangle = pygame.Surface((self.__width, self.__width))
+            rectangle.fill(self.__palette.purple)
+            rectangle.set_alpha(50)
+            # surface.blit(rectangle, (0, 0))
+            surface.blit(rectangle, (self.__x_coordinate, self.__y_coordinate))
 
-    def get_neighbors(self):
+    @staticmethod
+    def get_cell(x_coordinate: int, y_coordinate: int, column_count: int=10)->int:
+        """
+        Gets the index of a given cell from the given set of coordinates
+        :param (int) x_coordinate: the x coordinate of the base cell
+        :param (int) y_coordinate: the y coordinate of the base cell
+        :param (int) column_count: the number of columns that the grid has
+        :return: (int) the index of the specific cell in the grid
+        """
+        return x_coordinate + y_coordinate * column_count
+
+    def get_neighbors(self, cells):
+        indexes = [self.get_cell(self.__x_coordinate, self.__y_coordinate + 1),
+                   self.get_cell(self.__x_coordinate, self.__y_coordinate - 1),
+                   self.get_cell(self.__x_coordinate - 1, self.__y_coordinate),
+                   self.get_cell(self.__x_coordinate + 1, self.__y_coordinate)]
+
         neighbors = []
+
+        for index in indexes:
+            if not cells[index].visited:
+                neighbors.append(cells[index])
+
+        return random.choice(neighbors)
 
 
 
--- MazeObjects/Maze.py
+++ MazeObjects/Maze.py
@@ -5,9 +5,10 @@ import pygame; import random
 class Maze:
     def __init__(self):
         surface_height, surface_width = 400, 400  # The dimensions of our py game window (height, width)
-        self.cell_width = surface_width // 10  # The width of our square shaped cell
+        self.cell_width = surface_width // 10  # The width and height of our square shaped cell
         self.rows, self.columns = surface_width // self.cell_width, surface_height // self.cell_width
         self.grid = []
+        self.current = None
 
         pygame.init()
         self.surface = pygame.display.set_mode((surface_height, surface_width))
@@ -18,8 +19,8 @@ class Maze:
                 grid_cell = Cell(row, column, self.cell_width)
                 self.grid.append(grid_cell)
 
-        self.grid[0].visited = True
-        self.grid[0].walls[1] = False
+        self.current = self.grid[0]
+        self.current.visited = True
 
     def build(self):
         while True:
@@ -31,7 +32,6 @@ class Maze:
             if self.grid:  # if there a cells in the grid, display them
                 for grid_cell in self.grid:
                     grid_cell.draw(self.surface)
-                    self.grid.pop()
 
                 pygame.display.flip()  # update the canvas so the grid will be shown after it is drawn
 
