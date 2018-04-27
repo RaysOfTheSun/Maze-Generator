838e80cfc0d28dd4927e8fff5a02210464d2bbee
RaysOfTheSun <rdelsol35@gmail.com>
2018-04-27 23:29:34 +0800

    update Pathfinder classes
    + Updated methods and attributes associated with the Pathfinding tile
    + Added an attribute to the Pathfinder class that would give it its own copy of the grid

--- PathFinding/Pathfinder.py
+++ PathFinding/Pathfinder.py
@@ -1,16 +1,18 @@
 import pygame
 from MazeObjects.Colors import Color
-
+from PathFinding.PathfindingTile import Tile
 
 class Pathfinder:
-    def __init__(self, initial_coordinates, goal_coordinates):
+    def __init__(self, initial_coordinates, goal_coordinates, grid):
         self.x_coordinate, self.y_coordinate = initial_coordinates
         self.x_goal, self.y_goal = goal_coordinates
         self.open_paths, self.closed_paths = [], []
+        self.paths = [Tile((cell.x_coordinate, cell.y_coordinate)) for cell in grid]
 
     def get_neighbors(self, x_coordinate, y_coordinate):
         neighbors = {'top': (x_coordinate, (y_coordinate + 1)), 'bottom': (x_coordinate, (y_coordinate + 1)),
                      'left': ((x_coordinate - 1), y_coordinate), 'right': ((x_coordinate + 1), y_coordinate)}
+
 
 
 
--- PathFinding/PathfindingTile.py
+++ PathFinding/PathfindingTile.py
@@ -1,6 +1,24 @@
 class Tile:
-    def __init__(self):
-        self.G_Score = 0
-        self.H_Score = 0
-        self.F_Score = 0
+    def __init__(self, coordinates):
+        """
+        Initializes an instance of the Tile class
+        :param coordinates: The position of the tile in the grid (Tuple)
+        """
+        self.G_Score = -1
+        self.H_Score = -1
+        self.F_Score = -1
+        self.x_coordinates, self.y_coordinates = coordinates
+
+    def compute_estimate(self, coordinates):
+        """
+        Computes the H score for the tile
+        The H score is the distance from the cell to the target cell disregarding obstacles
+        :param coordinates: The coordinates of the target cell in the grid (Tuple)
+        :return: The H score of the tile relative to the target cell
+        """
+        goal_x, goal_y = coordinates
+        return abs(((goal_x - self.x_coordinates) + (goal_y - self.y_coordinates)))  # Manhattan Distance Formula
+
+
+
 
