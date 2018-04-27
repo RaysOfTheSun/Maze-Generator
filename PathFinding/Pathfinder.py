176296c7a2cca5b3cb0eb760718e999f1e89372e
RaysOfTheSun <rdelsol35@gmail.com>
2018-04-27 14:38:54 +0800

    update classes
    + Updated pathfinder class
    + Added tile class

--- PathFinding/Pathfinder.py
+++ PathFinding/Pathfinder.py
@@ -3,13 +3,17 @@ from MazeObjects.Colors import Color
 
 
 class Pathfinder:
-    def __init__(self, x_coordinate, y_coordinate):
-        """
-        Initializes an instance of the Pathfinder class
-        :param x_coordinate: The initial position of the pathfinder in the x-axis
-        :param y_coordinate: The initial position of the pathfinder in the y-axis
-        """
-        self.x_coordinate = x_coordinate
-        self.y_coordinate = y_coordinate
-        self.closed_paths = []  # Closed list
-        self.open_paths = []  # Open list
+    def __init__(self, initial_coordinates, goal_coordinates):
+        self.x_coordinate, self.y_coordinate = initial_coordinates
+        self.x_goal, self.y_goal = goal_coordinates
+        self.open_paths, self.closed_paths = [], []
+
+    def get_neighbors(self, x_coordinate, y_coordinate):
+        neighbors = {'top': (x_coordinate, (y_coordinate + 1)), 'bottom': (x_coordinate, (y_coordinate + 1)),
+                     'left': ((x_coordinate - 1), y_coordinate), 'right': ((x_coordinate + 1), y_coordinate)}
+
+
+
+
+
+
---
+++ PathFinding/PathfindingTile.py
@@ -0,0 +1,6 @@
+class Tile:
+    def __init__(self):
+        self.G_Score = 0
+        self.H_Score = 0
+        self.F_Score = 0
+
